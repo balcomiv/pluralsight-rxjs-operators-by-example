854661e8869d35407e3713e5e7de4c4d209b8f32
BalcomIV <35431931+balcomiv@users.noreply.github.com>
2021-01-28 12:02:04 -0500

    add - catchError

--- src/app/errorHandling/catchError.ts
+++ src/app/errorHandling/catchError.ts
@@ -1,1 +1,55 @@
-//  Fix Me -- Left off here
+/**
+ * catchError
+ * prevent the error from stopping the stream
+ * replace the error with a new source
+ */
+
+import { of, throwError } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+
+console.log('# catch and rethrow the error');
+throwError('my error')
+  .pipe(
+    catchError((error) => {
+      console.log(`Caught an error: ${error}`);
+      return throwError(`Rethrown: ${error}!`);
+    }),
+    catchError((error) => {
+      console.log(error);
+      return of(null);
+    })
+  )
+  .subscribe(
+    (value) => {
+      if (value) {
+        console.log(value);
+      }
+    },
+    (error) => console.log('oops'),
+    () => console.log('complete')
+  );
+
+//  Output:
+//  Caught an error: my error
+//  Rethrown: my error!
+//  complete
+
+console.log(' Catch something undefined');
+of('a', 1)
+  .pipe(
+    map((v) => (v as string).toUpperCase()),
+    catchError((error) => of(null))
+  )
+  .subscribe(
+    (value) => {
+      if (value) {
+        console.log(value);
+      }
+    },
+    (error) => console.log('oops'),
+    () => console.log('complete')
+  );
+
+//  Output:
+//  A
+//  complete
