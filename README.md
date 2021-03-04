86e9ced23321ffbafe848979ca41d5447c55a896
BalcomIV <35431931+balcomiv@users.noreply.github.com>
2021-03-04 13:08:39 -0500

    update - switchMap

--- README.md
+++ README.md
@@ -10,3 +10,7 @@ This project was generated with [Angular CLI](https://github.com/angular/angular
 ## Pluralsight Link
 
 - https://app.pluralsight.com/course-player?clipId=943f5297-2bfb-4838-8c8d-958a9d2d077a
+
+## Other Really Cool Links
+
+- [Learn to combine RxJs sequences with super intuitive interactive diagrams](https://indepth.dev/posts/1114/learn-to-combine-rxjs-sequences-with-super-intuitive-interactive-diagrams)
--- src/app/value-transformation/switchMap.ts
+++ src/app/value-transformation/switchMap.ts
@@ -1,2 +1,76 @@
 //  Subscribe to observable, every time source emits
 //  When source emits, we unsubscribe from the previous generated observable
+
+import { interval, Observable, timer } from 'rxjs';
+import { map, switchMap, take, tap } from 'rxjs/operators';
+
+const character = new Observable((subscriber) => {
+  subscriber.next('A');
+  setTimeout(() => subscriber.next('B'), 200);
+  setTimeout(() => subscriber.complete(), 300);
+});
+
+const numberInterval = interval(50).pipe(take(5));
+
+//  Switch to new inner observable when source emits
+console.log('# switch to a new source');
+character
+  .pipe(
+    tap((x) => console.log(`Value from source: ${x}`)),
+    switchMap(() => numberInterval) //   <-- here we subscribe to numberInterval
+  )
+  .subscribe(
+    (value) => console.log(`emitted value: ${value}`),
+    (err) => console.log(err),
+    () => console.log('Complete')
+  );
+
+// Value from source: A ​​​​​
+// emitted value: 0 ​​​​​
+// emitted value: 1 ​​​​​
+// emitted value: 2 ​​​​​
+
+// Value from source: B ​​​​​
+// emitted value: 0 ​​​​​
+// emitted value: 1 ​​​​​
+// emitted value: 2 ​​​​​
+// emitted value: 3 ​​​​​
+// emitted value: 4 ​​​​​
+// Complete
+
+//  timer(0, 150).pipe(take(3), (char, num) => `${char}${num}`)).subscribe(val => console.log(val));
+
+//  Just to delay example
+setTimeout(() => {
+  character
+    .pipe(
+      switchMap(
+        () => timer(0, 150).pipe(take(3)),
+        (char, num) => `Val: ${char}${num}`
+      )
+    )
+    .subscribe(
+      (val) => console.log(val),
+      (err) => console.log(err),
+      () => console.log('complete')
+    );
+}, 1000);
+
+//  Non-deprecated Version
+//  Just to delay example
+setTimeout(() => {
+  character
+    .pipe(
+      switchMap((char) => {
+        return timer(0, 150).pipe(
+          take(3),
+          map((num) => `Val: ${char}${num}`)
+        );
+      })
+    )
+    .subscribe(
+      (val) => console.log(val),
+      (err) => console.log(err), // ?
+      () => console.log('complete')
+    );
+}, 2000);
